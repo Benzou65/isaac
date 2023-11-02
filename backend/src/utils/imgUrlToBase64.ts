cfd841da16b8894cd59be217b0df86efeb25aa70
Lionel Bonzoumet <lionel.bonzoumet@gmail.com>
2023-11-02 21:58:31 +0100

    feat: add image extension detection

--- backend/src/utils/imgUrlToBase64.ts
+++ backend/src/utils/imgUrlToBase64.ts
@@ -6,8 +6,23 @@ export const imgUrlToBase64 = async (url: string) => {
     const response = await axios.get(url, { responseType: 'arraybuffer' })
     const base64 = Buffer.from(response.data, 'binary').toString('base64')
 
-    return `data:image/png;base64,${base64}`
+    const extension = getExtension(url)
+
+    return `data:image/${extension};base64,${base64}`
   } catch (error) {
     throw new Error('Failed to convert image URL to base64')
   }
+}
+
+const getExtension = (url: string) => {
+  const regexPng = /\.png/
+  const regexGif = /\.gif/
+  const regexJpg = /\.jpe?g/
+  const regexSvg = /\.svg/
+
+  if (regexPng.test(url)) return 'png'
+  if (regexGif.test(url)) return 'gif'
+  if (regexJpg.test(url)) return 'jpg'
+  if (regexSvg.test(url)) return 'svg'
+  return 'png'
 }
